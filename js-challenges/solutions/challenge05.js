c39862a2488f4bcd93da0999eeb01f786b439a92
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-24 16:03:55 +0100

    fix c05 NaN output

--- js-challenges/solutions/challenge05.js
+++ js-challenges/solutions/challenge05.js
@@ -11,12 +11,11 @@ for (numCounter=1; numCounter<=10; numCounter++) {
 */
 
 //With loop and arrayList
-/*
+
 let totalSum = 1;
 let listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (i=0; i<10; i++) {
+for (i=0; i<listaNumeros.length; i++) {
     totalSum = totalSum + listaNumeros[i];
     console.log(totalSum);
 }
-*/
