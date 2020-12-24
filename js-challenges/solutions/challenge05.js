536a6225cf61590350027d22d30c587f14ec6e46
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-24 15:58:30 +0100

    add challenge05 solution
    the solution implements two versions: with and without an arrayList.

--- js-challenges/solutions/challenge05.js
+++ js-challenges/solutions/challenge05.js
@@ -1,8 +1,22 @@
 //Coding challenge #5: Calculate the sum of numbers from 1 to 10
 
-let totalSum = 0;
+//Without arrayList
+/*
+let totalSum = 1;
 
-for (i=1; i<=10; i++) {
-    totalSum += i;
+for (numCounter=1; numCounter<=10; numCounter++) {
+    console.log(`${totalSum} + ${numCounter} = ${totalSum + numCounter}`);
+    totalSum += numCounter;
+}
+*/
+
+//With loop and arrayList
+/*
+let totalSum = 1;
+let listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i=0; i<10; i++) {
+    totalSum = totalSum + listaNumeros[i];
     console.log(totalSum);
-};
+}
+*/
