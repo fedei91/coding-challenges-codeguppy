c2d8c5b4316b2b392c90df07f9a3826ac7e0ca90
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-29 19:39:23 +0100

    implement a function to solve c10.js
    comment previous solution

--- js-challenges/solutions/challenge10.js
+++ js-challenges/solutions/challenge10.js
@@ -1,5 +1,26 @@
 //Calculate the sum of numbers in an array of numbers
 
+//implement function into solution
+function sumArray(randomArray){
+    for (i=0; i<randomArray.length; i++) {
+        //calculate the sum of it's numbers
+        totalSum += randomArray[i];
+    }
+    return totalSum;
+}
+
+let count = 0;
+let totalSum = 0;
+let randomArray = [];
+
+while (count < 10) {
+    let randomNumber = parseInt(prompt('Enter a random number'));
+    randomArray.push(randomNumber);
+    count++;
+}
+
+console.log(sumArray(randomArray));
+
 /*
 //first fill an array
 let count = 0;
