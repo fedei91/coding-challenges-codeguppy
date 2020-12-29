4d1c8895c34c68edaae6b29432c1be9b1a0d4c3c
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-29 17:08:51 +0100

    resolve celsius/farenheit challenges

--- js-challenges/solutions/challenge08.js
+++ js-challenges/solutions/challenge08.js
@@ -1,3 +1,9 @@
 //Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
 
 // f Fahrenheit (°F) = (Celsius x 1.8) + 32
+
+function toFarenheit (numCelsius) {
+    return (numCelsius * 1.8 + 32);
+}
+
+console.log(toFarenheit(30));
--- js-challenges/solutions/challenge09.js
+++ js-challenges/solutions/challenge09.js
@@ -1,3 +1,9 @@
 //Create a function that will convert from Fahrenheit to Celsius
 
 // f Celsius (°C) = (Fahrenheit - 32) / 1.8
+
+function toCelsius (numFarenheit) {
+    return (numFarenheit - 32) / 1.8;
+}
+
+console.log(toCelsius(30));
