67d4b31bdaae18ee7f4dc616a8da8eb49c34d0b2
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-29 19:38:42 +0100

    solve c10.js

---
+++ js-challenges/solutions/challenge10.js
@@ -0,0 +1,24 @@
+//Calculate the sum of numbers in an array of numbers
+
+/*
+//first fill an array
+let count = 0;
+let totalSum = 0;
+let randomNumber = 0
+let randomArray = [];
+
+while (count < 10) {
+    randomNumber = Math.floor(Math.random() * 10);
+    randomArray.push(randomNumber);
+    count++;
+}
+
+//go through the array
+for (i=0; i<randomArray.length; i++) {
+    //calculate the sum of it's numbers
+    totalSum += randomArray[i];
+}
+
+//show the result
+console.log(totalSum);
+*/
