3b0866471657ebbae4e0ae68064f8f9089f500eb
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-12-29 23:33:10 +0100

    solve challenge11.js

--- js-challenges/solutions/challenge11.js
+++ js-challenges/solutions/challenge11.js
@@ -1,1 +1,21 @@
 //Coding challenge #11: Calculate the average of the numbers in an array of numbers
+
+//create an array list
+randomArray = [];
+let totalAverage = 0;
+let count = 0;
+
+while (count < 10) {
+    let randomNumber = parseInt(prompt('Enter a random number'));
+    randomArray.push(randomNumber);
+    count++;
+    totalAverage += randomNumber;
+}
+
+//console.log(randomArray);
+
+function calculateAverage(totalAverage, randomArray) {
+    return totalAverage / randomArray.length;
+}
+
+console.log(calculateAverage(totalAverage, randomArray));
